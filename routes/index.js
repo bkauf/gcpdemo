7f48d2b24db568bfd6b3e741d19d94130957fe5d
bkauf@google.com <bkauf@google.com>
2019-07-08 10:03:25 -0400

    include build ID in index page

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,16 @@ var express  = require('express');
 var router   = express.Router();
 var os       = require("os");
 var hostname = os.hostname();
+var fs       = require('fs');
 /* GET home page. */
+fs.readFile('buildID', 'utf8', function(err, contents) {
+    var buildID = contents;
+});
+
 router.get('/', function(req, res, next) {
 
 
-  res.render('index', { title: '::Cloud Tester::', container: hostname });
+  res.render('index', { title: '::Cloud Tester::', container: hostname, buildID: buildID });
 });
 
 module.exports = router;
